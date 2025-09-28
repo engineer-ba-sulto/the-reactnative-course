605124e1658bf2f85293e06b04dd468b37ed831a
Hiroki Chiyoda <contact@arafipro.com>
2025-09-28 22:00:30 +0900

    ダッシュボードサイドバーコンポーネントのリンクに動的スタイルを追加
    - usePathname フックを使用して現在のパスを取得し、選択中のメニューアイテムに背景色を適用
    - SidebarMenu のクラス名を修正し、スタイルを整理

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -12,19 +12,26 @@ import {
 } from "@/components/ui/sidebar";
 import { dashboardMenuItems } from "@/constants/dashboardMenuItems";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function DashboardSidebar({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
   return (
     <Sidebar {...props} className="mt-18">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
-            <SidebarMenu className="p-5">
+            <SidebarMenu className="">
               {dashboardMenuItems.map((item) => (
                 <SidebarMenuItem key={item.title} className="py-1">
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton
+                    asChild
+                    className={`rounded-md p-4 ${
+                      pathname === item.url ? "bg-gray-300" : ""
+                    }`}
+                  >
                     <Link href={item.url} className="flex items-center gap-2">
                       <item.icon className="h-4 w-4" />
                       {item.title}
