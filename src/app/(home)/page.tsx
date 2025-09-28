008aacd3b74f460eede91aeb9190c5d1220176f3
Hiroki Chiyoda <contact@arafipro.com>
2025-09-28 18:58:08 +0900

    ホームページのメタデータを追加
    - src/app/(home)/page.tsx にホームページのタイトルを設定するメタデータを追加

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
 import AppsList from "@/components/AppList";
 import ArticlesList from "@/components/ArticlesList";
 import Hero from "@/components/Hero";
+import { Metadata } from "next";
 // import WaitingList from "@/components/WaitingList";
+
+export const metadata: Metadata = {
+  title: "ホーム",
+};
 
 export default function Home() {
   return (
